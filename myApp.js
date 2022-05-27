a6c0f3bc2eba6d9afbd696784c88cfe9c659e086
Edrick Vince Sinsuan <61485817+ederekun@users.noreply.github.com>
2022-05-27 21:54:13 +0000

    Update myApp.js with current progress
    This commit includes changes made according to fCC lessons until the simple logger section.

--- myApp.js
+++ myApp.js
@@ -1,6 +1,30 @@
 let express = require('express');
 let app = express();
 
+// app.get('/', (req, res) => res.send('Hello Express'));
+
+const pathAbsolute = (path) => __dirname + path;
+
+// Simple logger
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
+app.use(express.static(pathAbsolute('/public')));
+app.use('/public', express.static(pathAbsolute('/public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(pathAbsolute('/views/index.html'));
+});
+
+app.get('/json', (req, res) => {
+  res.json(process.env.MESSAGE_STYLE === 'uppercase' ? {
+      "message": "HELLO JSON"
+    } : {
+      "message": "Hello json"
+  });
+});
 
 
 
@@ -36,4 +60,7 @@ let app = express();
 
 
 
- module.exports = app;
+
+
+
+module.exports = app;
